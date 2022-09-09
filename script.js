193902a53a76751dd2eefccf706c1446d0062e2f
Monet <shelbyhernandez9876@yahoo.com>
2022-09-08 23:55:43 -0500

    began working on adding tailwind classes, added google fonts, and JS functions

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-var weatherAPI ='https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}'
+// Variables used for search and search history functions
+const searchFormEl = $('#searchform');
+const searchCity = $('#locationsearch');
+const searchCityButton = $('#searchbtn');
+const searchHistoryEl = $('#search-history');
+
+function searchCityValue(event) {
+    event.preventDefault();
+
+    const searchCityText = $('#locationsearch').value.trim();
+
+    if (searchCityText === "") {
+        console.error('Please enter a city');
+        return;
+    }
+}
+
+function displaySearchHistory(){}
+
+
+searchCityButton.addEventListener('click', searchCityValue);
+
+searchHistoryEl.addEventListener('click', displaySearchHistory);
+
+const weatherAPI ='https://api.openweathermap.org/data/2.5/weather?q=" + searchCityText + "&units=imperial&appid=073e596cca8ed71b557304d86f8bfbdc';
