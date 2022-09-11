53ca1fea12ab6768612d711b852ad4696eb144cd
Monet <shelbyhernandez9876@yahoo.com>
2022-09-11 15:10:17 -0500

    replaced tailwind with bootstrap cdn, modified README.md, added current weather function to JS

--- README.md
+++ README.md
@@ -3,19 +3,10 @@
 
 ## Description
 
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
+- This Weather Dashboard served as a means of better utilizing JavaScript to perform API requests and work with server-side API's to fetch and render specific data to depict a weather forecaster.
 
 
 ## Demo
-
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
-
-## Usage
 
 Provide instructions and examples for use. Include screenshots as needed.
 
@@ -29,11 +20,6 @@ To add a screenshot, create an `assets/images` folder in your repository and upl
 
 - Favicon created by favicon.io (https://favicon.io)
 
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
 
 ## License
 
@@ -62,18 +48,8 @@ SOFTWARE.
 
 ## Badges
 
-![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+![reposizebadge](https://img.shields.io/github/repo-size/shernandez927/rain-man?style=for-the-badge) ![languagebadge](https://img.shields.io/github/languages/count/shernandez927/rain-man?style=for-the-badge) ![licensebadge](https://img.shields.io/github/license/shernandez927/rain-man?style=for-the-badge)
 
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
 
-## Features
 
-If your project has a lot of features, list them here.
 
-## How to Contribute
-
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-
-## Tests
-
-Go the extra mile and write tests for your application. Then provide examples on how to run them here.
--- script.js
+++ script.js
@@ -1,25 +1,78 @@
 // Variables used for search and search history functions
-const searchFormEl = $('#searchform');
-const searchCity = $('#locationsearch');
-const searchCityButton = $('#searchbtn');
-const searchHistoryEl = $('#search-history');
+const apiKey = "554f6e2a0ea6794e7db1dd1d10c95025";
+const searchCityEl = document.querySelector('#locationsearchinput');
+// const searchInput = document.querySelector('#locationsearchinput').value.trim();
+const searchFormEl = document.querySelector('#searchform');
+const searchCityButton = document.querySelector('#searchbtn');
+const resultTextEl = document.querySelector('#result-text');
+const currentForecastEl = document.querySelector('#current-forecast');
+// Variables to append new elements to
+const currentWeatherEl = document.querySelector('#current-weather');
+const fiveDayEl = document.querySelector('#five-day-forecast');
+const searchHistoryEl = document.querySelector('#search-history');
+// const apiURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&appid=" +apiKey + "&units=imperial";
 
-function searchCityValue(event) {
+
+    function handleSearchFormSubmit(event) {
     event.preventDefault();
 
-    const searchCityText = $('#locationsearch').value.trim();
+    // Variable to store city text input as parameter used in API url
+    const searchInput = document.querySelector('#locationsearchinput').value.trim();
 
-    if (searchCityText === "") {
-        console.error('Please enter a city');
+    // If conditional statement to console an error if empty text input is submitted
+    if (!searchInput) {
+        console.error("Please enter a City");
         return;
     }
+
+    getWeather(searchInput);
 }
 
-function displaySearchHistory(){}
+    function getWeather(searchInput) {
+      const apiURL =
+        "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + "&appid=" + apiKey + "&units=imperial";
+      fetch(apiURL)
+        .then(function (response) {
+          if (!response.ok) {
+            throw response.json();
+          }
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data)
+          displayCurrentWeather(data)
+        });
+
+    }
+
+    // function to append current weather data to create and append text and weather icon to current weather body element
+    function displayCurrentWeather(data) {
+
+        const resultName = document.createElement('h2');
+        resultName.classList.add("text-center", "p-2");
+        resultName.textContent = "Showing weather for: " + data.city.name;
+        currentWeatherEl.append(resultName);
+
+        const resultIcon = document.createElement('img');
+        resultIcon.src = "http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png";
+        currentWeatherEl.append(resultIcon);
+
+        const resultTemp = document.createElement('p')
+        resultTemp.textContent = "Temperature: " + data.list[0].main.temp.toFixed(1) + "\u2109";
+        currentWeatherEl.append(resultTemp)
+
+        const resultWind = document.createElement('p');
+        resultWind.textContent = "Wind Speed: " + data.list[0].wind.speed.toFixed(1) + " MPH";
+        currentWeatherEl.append(resultWind);
+
+        const resultHumidity = document.createElement('p');
+        resultHumidity.textContent = "Humidity: " + data.list[0].main.humidity + "%";
+        currentWeatherEl.append(resultHumidity);
+    
+    }
 
 
-searchCityButton.addEventListener('click', searchCityValue);
+// event listener to run function
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
 
-searchHistoryEl.addEventListener('click', displaySearchHistory);
-
-const weatherAPI ='https://api.openweathermap.org/data/2.5/weather?q=" + searchCityText + "&units=imperial&appid=073e596cca8ed71b557304d86f8bfbdc';
+// searchHistoryEl.addEventListener('click', displaySearchHistory);
